ed180d4ff0238f7458224108f3bacac3787264b3
aniketsrivastava0011 <61534670+aniketsrivastava0011@users.noreply.github.com>
2022-03-11 23:26:28 +0530

    Update create_iam_role.sh

--- eks/create_iam_role.sh
+++ eks/create_iam_role.sh
@@ -7,12 +7,6 @@ aws iam create-role --role-name CodeBuildKubectlRole --assume-role-policy-docume
 
 aws iam put-role-policy --role-name CodeBuildKubectlRole --policy-name eks-describe --policy-document file:///tmp/iam-role-policy
 
-aws iam attach-role-policy --role-name CodeBuildKubectlRole --policy-arn arn:aws:iam::aws:policy/CloudWatchLogsFullAccess
-
 aws iam attach-role-policy --role-name CodeBuildKubectlRole --policy-arn arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess
-
-aws iam attach-role-policy --role-name CodeBuildKubectlRole --policy-arn arn:aws:iam::aws:policy/AWSCodeCommitFullAccess
-
-aws iam attach-role-policy --role-name CodeBuildKubectlRole --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
 
 aws iam attach-role-policy --role-name CodeBuildKubectlRole --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess
